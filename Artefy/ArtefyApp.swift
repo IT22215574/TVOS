97c49501fbb6ee7e6027814324cbe2fa9878317f
Ravindu <it22215574@my.sliit.lk>
2025-11-16 20:02:03 +0530

    1st commit

---
+++ Artefy/ArtefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtefyApp.swift
+//  Artefy
+//
+//  Created by Anupa Herath on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreen()
+        }
+    }
+}
